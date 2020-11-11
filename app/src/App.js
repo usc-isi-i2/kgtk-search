1caac2a15117837c205a6c4d4af13390933388f3
g1eb <mail@g1eb.com>
2020-11-11 10:57:31 -0800

    Add the baseline theme styling for the app

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,37 @@
 import React from 'react'
+import Container from '@material-ui/core/Container'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { withStyles, createMuiTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles'
+
+
+let theme = createMuiTheme()
+theme = responsiveFontSizes(theme)
+
+
+const styles = theme => ({
+  '@global': {
+    body: {
+      background: 'linear-gradient(150deg, #C0C0C0, #708090)',
+      backgroundSize: '100% 150%',
+      padding: theme.spacing(3, 1),
+      height: '100vh',
+    },
+  },
+})
 
 
 class App extends React.Component {
 
   render() {
     return (
-      <div>
-      </div>
+      <ThemeProvider theme={theme}>
+        <Container maxWidth="xl">
+          <CssBaseline />
+        </Container>
+      </ThemeProvider>
     )
   }
 }
 
 
-export default App
+export default withStyles(styles)(App)
