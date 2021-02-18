b6f99b53795bea5a1bf38a036b15d901008e73b2
saggu <amandeep.s.saggu@gmail.com>
2021-02-18 14:22:52 -0800

    statements to result class

--- iwqs/search/search_result.py
+++ iwqs/search/search_result.py
@@ -3,13 +3,14 @@ from typing import List
 
 class SearchResult(object):
     def __init__(self, qnode: str, label: List[str], alias: List[str], description: List[str], pagerank: float,
-                 data_type: str = None):
+                 data_type: str = None, statements: int = 1):
         self.qnode = qnode
         self.label = label
         self.alias = alias
         self.description = description
         self.pagerank = pagerank
         self.data_type = data_type
+        self.statements = statements
 
     def to_json(self, extra_info=False):
         r = {
@@ -20,6 +21,7 @@ class SearchResult(object):
             r['label'] = self.label
             r['alias'] = self.alias
             r['pagerank'] = self.pagerank
+            r['statements'] = self.statements
             if self.data_type:
                 r['data_type'] = self.data_type
         return r
