b8599804ddf5c45d0fa243d1aeec336abf24fe12
saggu <amandeep.s.saggu@gmail.com>
2021-02-24 11:02:58 -0800

    check if its a production environment

--- iwqs/search/main.py
+++ iwqs/search/main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from flask import current_app
 from flask_restful import Resource
@@ -26,22 +27,25 @@ class FindNearestQnodes(Resource):
         size = request.args.get('size', 20)
         extra_info = request.args.get('extra_info', 'false').lower().strip() == 'true'
 
-        # Log the search in our mongo db
-        timestamp = datetime.now().isoformat()
-        new_entry = current_app.mongo.db.search.insert_one({
-            'created_at': timestamp,
-            'search_term': search_term,
-            'extra_info': extra_info,
-            'query_type': query_type,
-            'instance_of': instance_of,
-            'is_class': is_class,
-            'lowercase': language,
-            'user_es_url': user_es_url,
-            'user_es_index': user_es_index,
-            'language': language,
-            'item': item,
-            'size': size,
-        })
+        is_production = os.environ.get('KGTK_SEARCH_PRODUCTION', 'false').lower() == 'true'
+
+        if is_production:
+            # Log the search in our mongo db
+            timestamp = datetime.now().isoformat()
+            current_app.mongo.db.search.insert_one({
+                'created_at': timestamp,
+                'search_term': search_term,
+                'extra_info': extra_info,
+                'query_type': query_type,
+                'instance_of': instance_of,
+                'is_class': is_class,
+                'lowercase': language,
+                'user_es_url': user_es_url,
+                'user_es_index': user_es_index,
+                'language': language,
+                'item': item,
+                'size': size,
+            })
 
         if is_class:
             query = es_search.create_ngram_query(search_term, size=size, language=language, instance_of='',
