53ed6dd370ca4e977da1f1e4ebf9aafadfc0bede
g1eb <mail@g1eb.com>
2021-02-24 01:01:01 -0800

    Initialize our slack client on the flask app with a token from the environment

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 from flask_pymongo import PyMongo
+from slackclient import SlackClient
 from iwqs.search.main import FindNearestQnodes
 from app_config import host, port
 
@@ -16,6 +17,10 @@ app.secret_key = os.environ.get('APP_SECRET', '')
 MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://0.0.0.0:27017/kgtk')
 app.config["MONGO_URI"] = MONGO_URI
 app.mongo = PyMongo(app)
+
+# Slack api settings
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN', '')
+app.slack_client = SlackClient(SLACK_TOKEN)
 
 api = Api(app)
 api.add_resource(FindNearestQnodes, '/api/<string:search_term>')
