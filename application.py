3a524752962183eb381c3953dc3244c161d9ec80
g1eb <mail@g1eb.com>
2021-02-26 23:43:27 -0800

    Switch out the slack client for the slack webclient

--- application.py
+++ application.py
@@ -3,27 +3,33 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
 from flask_pymongo import PyMongo
-from slackclient import SlackClient
 from iwqs.search.main import FindNearestQnodes
 from app_config import host, port
+from slack import WebClient
+
 
 app = Flask(__name__)
 CORS(app)
 
+
 # Set the APP_SECRET key
 app.secret_key = os.environ.get('APP_SECRET', '')
+
 
 # Add mongo db settings for logging
 MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://0.0.0.0:27017/kgtk')
 app.config["MONGO_URI"] = MONGO_URI
 app.mongo = PyMongo(app)
 
+
 # Slack api settings
 SLACK_TOKEN = os.environ.get('SLACK_TOKEN', '')
-app.slack_client = SlackClient(SLACK_TOKEN)
+app.slack_client = WebClient(SLACK_TOKEN)
+
 
 api = Api(app)
 api.add_resource(FindNearestQnodes, '/api/<string:search_term>')
+
 
 if __name__ == '__main__':
     app.run(host=host, port=port)
