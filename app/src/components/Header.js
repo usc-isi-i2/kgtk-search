a73bfb2c5df8cae8e70cbcf56221b195936e0ff9
g1eb <mail@g1eb.com>
2021-09-25 13:11:50 -0700

    Update header component to look similar to the kgtk-browser header

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 
@@ -6,16 +8,16 @@ import Logo from './Logo'
 
 
 const useStyles = makeStyles(theme => ({
-  header: {
-    color: '#fefefe',
-    marginTop: theme.spacing(3),
-  },
-  logo: {
-    display: 'inline-block',
-    verticalAlign: 'middle',
-    width: theme.spacing(12),
-    height: theme.spacing(12),
+  menuIcon: {
+    width: theme.spacing(8),
+    height: theme.spacing(8),
     marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
   },
 }))
 
@@ -25,17 +27,21 @@ const Header = () => {
   const classes = useStyles()
 
   return (
-    <Typography
-      component="h3"
-      variant="h3"
-      className={classes.header}>
-      <a href="https://github.com/usc-isi-i2/kgtk" title="Knowledge Graph Toolkit" rel="noopener noreferrer nofollow" target="_blank">
-        <div className={ classes.logo }>
-          <Logo/>
+    <AppBar position="static">
+      <Toolbar>
+        <div className={classes.menuIcon}>
+          <a href="https://github.com/usc-isi-i2/kgtk"
+            title="Knowledge Graph Toolkit"
+            rel="noopener noreferrer nofollow"
+            target="_blank">
+            <Logo/>
+          </a>
         </div>
-      </a>
-      Knowledge Graph Text Search
-    </Typography>
+        <Typography className={classes.title} variant="h6" noWrap>
+          Knowledge Graph Text Search
+        </Typography>
+      </Toolbar>
+    </AppBar>
   )
 }
 
